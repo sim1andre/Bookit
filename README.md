0e838da63e25b0beb646dba1afb631667f397608
heroyv <oyvindher@gmail.com>
2015-11-28 17:33:14 +0100

    bunch of changes.. and failed attempt on auth

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# ExpressJS-mal
+# Book-it
+### Booking service app built in ExpressJS
 
 ## Installasjon
-1. `$ git clone http://github.com/heroyv/express-mal.git`
+1. `$ git clone http://github.com/sim1andre/Bookit.git`
 2. `$ npm install`
 3. Åpne et nytt terminalvindu og skriv: `$ sudo mongod`
 4. `$ gulp`
-5. $ BOYYA.
-# Bookit 
--- config/routes.js
+++ config/routes.js
@@ -2,16 +2,31 @@
 
 const Bookings = require('./../api/controllers/bookings');
 
-module.exports = (app) => {
+module.exports = (app, passport) => {
   app.get('/', (req, res) => {
     res.render('home');
   })
+
+  app.get('/profile', (req, res) => {
+    res.render('profile');
+  })
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
   app.get('/bookings/create', (req, res) => {
     res.render('create');
   })
 
+  app.get('/login', (req, res) => {
+    res.render('login');
+  })
 
+  app.get('/signup', (req, res) => {
+    res.render('register');
+  })
 
   //GET
   app.get('/bookings', Bookings.findBookings);
